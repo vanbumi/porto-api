dcaee2d31c29f9b018cc8a2fdbd744d06093ec7f
vanbumi <jsp.dio@gmail.com>
2020-06-21 14:18:43 +0700

    can not upload image strapi-claudinary

---
+++ extensions/upload/config/plugins.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: "medio",
+      api_key: "478742828152457",
+      api_secret:
+        "CLOUDINARY_URL=cloudinary://478742828152457:8TBMfMxCkMO6x_2_XCVtClJ3N60@medio",
+    },
+  },
+});
